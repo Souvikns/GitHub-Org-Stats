d5d2e6910685c283ebc264da3c154c63872a54de
Souvikns <souvikde.ns@gmail.com>
2021-11-24 00:54:37 +0530

    feat: adding octokit for loading github's data

---
+++ api/github.js
@@ -0,0 +1,17 @@
+const { Octokit } = require('octokit');
+
+module.exports = GithubAPI
+
+class GithubAPI {
+    constructor(orgName) {
+        this.org = orgName;
+        this._octokit = new Octokit();
+    }
+    
+    async repos(){
+        const {data: repoList} = await this._octokit.request('GET /orgs/{org}/repos', {org: this.org});
+        return {
+            count: repoList.length()
+        }
+    }
+}
