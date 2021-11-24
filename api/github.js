de507b5f902b4a641569706911481f8095f4d7ed
souvik <souvikde.ns@gmail.com>
2021-11-24 12:24:45 +0530

    feat: setting up octokit  (#6)
    * feat: adding octokit for loading github's data
    
    * feat(test): set up jest for testing
    
    * fix(deepsource): fixing deepsource bugs
    
    * fix: minor fixes
    
    * fix(deepsource): fixing minor bugs

---
+++ api/github.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { Octokit } = require('octokit');
+
+class GithubAPI {
+    constructor(orgName) {
+        this.org = orgName;
+        this._octokit = new Octokit();
+    }
+    
+    async repos(){
+        const {data: repoList} = await this._octokit.request('GET /orgs/{org}/repos', {org: this.org});
+        return {
+            count: repoList.length
+        }
+    }
+}
+
+module.exports = GithubAPI
---
+++ tests/github.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const GithubAPI = require('../api/github');
+
+const githubAPI = new GithubAPI('asyncapi');
+
+describe("githubAPI should", () => {
+    test('load number of repos', async () => {
+        const {count} = await githubAPI.repos();
+        expect(count).toBeDefined();
+    })
+})
