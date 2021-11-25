94c4c1d9aa50049ced40790ee859f91b6b073816
Souvikns <souvikde.ns@gmail.com>
2021-11-25 16:26:33 +0530

    feat: integrating d3js to generate svg image
    see also #13

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require('express');
+import express from 'express'
+import { JSDOM } from 'jsdom';
+import * as d3 from 'd3';
 
 const app = express();
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     return res.status(200).send("<h1 align=\"center\">Welcome to GitHub ORG Stats API.</h1>")
 })
 
-module.exports = app;
+app.get('/dashboard', (req, res) => {
+    const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
+    let body = d3.select(dom.window.document.querySelector("body"))
+    let svg = body.append('svg')
+        .attr('width', 100)
+        .attr('height', 100)
+        .attr('xmlns', 'http://www.w3.org/2000/svg');
+
+    svg.append("rect")
+        .attr('x', 10)
+        .attr('y', 10)
+        .attr('width', 80)
+        .attr('height', 80)
+        .style('fill', 'orange')
+
+    res.setHeader('Content-Type', 'image/svg+xml')
+    res.send(body.html())
+});
+
+
+export default app
